28b9619267858666a1babcf1551d67b380897005
hjyang072 <hjyang072@gmail.com>
2025-08-14 13:43:55 +0900

    푸는 중 ..

--- members/양혜지/swea/D2/swea_1974_sudoku.py
+++ members/양혜지/swea/D2/swea_1974_sudoku.py
@@ -1,6 +1,8 @@
 # 스도쿠 검증
 # 가로 세로 3X3 중복 안됨
 
+# 초기화 -> 초기선언 row col sc
+
 T = int(input())
 for tc in range(1, T+1):
     arr = [list(map(int,input().split())) for _ in range(9)]
@@ -10,12 +12,14 @@ for tc in range(1, T+1):
         for j in range(9):
             # 행 검사
             row_arr = set(arr[i])
+            # 열 검사
             col_arr = set
             
             if len(set(row_arr))!=9 or len(col_arr)! =9:
     
     
 
-
+    # 3x3 검사 0,3,6 기준으로 9칸씩 검사
+    
 
     print(f'#{tc} {}')
